13499adc81c9f82dff0f356a2d37b7991a2b0f80
Himanshu <himanshupandey1036@gmail.com>
2023-07-02 16:21:35 +0530

    User created

--- server.js
+++ server.js
@@ -1,7 +1,44 @@
-const express = require("express")
-const serverConfig = require("./configs/server.config")
+const express = require("express");
+const mongoose = require("mongoose");
+const dbConfig = require("./configs/db.config");
+const serverConfig = require("./configs/server.config");
+const userModel = require('./model/user.model')
 const app = express();
 
-app.listen(serverConfig.PORT,() => {
-  console.log(`Server Started on the port number is ${serverConfig.PORT}`);
-})
+/**
+ * Logic to connect to MongoDb and create an ADMIN user
+ */
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection;
+db.on("error", () => {
+  console.log("Error while connecting to db");
+});
+
+db.once("open", () => {
+  console.log("DB is connected");
+  init();
+});
+
+async function init() {
+  /**
+   * Initialize the mongo db
+   * 
+   * Need to create the ADMIN user
+   */
+
+  const admin = await userModel.create({
+    name : "Himanshu",
+    userId : 1,
+    email : "himanshupandey1036@gmail.com",
+    password : "Himanshu2003",
+    userType : 1,
+  })
+
+  console.log(admin);
+}
+
+app.listen(serverConfig.PORT, () => {
+  console.log(
+    `Server Started on the port number is ${serverConfig.PORT}`
+  );
+});
